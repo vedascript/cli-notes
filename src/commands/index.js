120efcb56580b3c31f99e6e25f6634e08100ca75
vedansh <me.vedanshwani@gmail.com>
2023-12-05 19:22:23 +0530

    added a validation for note: only strings are allowed

--- src/commands/index.js
+++ src/commands/index.js
@@ -2,6 +2,33 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 yargs(hideBin(process.argv))
-  .command("t <text>", "", () => console.log("printing"))
+  .command(
+    "new <content>",
+    "Create a new note",
+    (yargs) => {
+      return yargs.positional("create-note", {
+        type: "string",
+        description: "A command to create new note",
+      });
+    },
+    (argv) => console.log("argv: ", argv.content)
+  )
+  .option("tags", {
+    alias: "t",
+    describe: "tags for the note.",
+    type: "string",
+  })
+  .coerce("content", (arg) => {
+    if (typeof arg !== "string") {
+      throw new Error("Input must be string.");
+    }
+    return arg;
+  })
+  .fail((msg) => {
+    if (msg) {
+      console.error(msg);
+      process.exit();
+    }
+  })
   .demandCommand(1)
   .parse();
